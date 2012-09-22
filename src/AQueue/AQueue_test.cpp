f360befc0f141cb15e38fcfa21ac1486aabc6626
Travace Reese <tsuk13@gmail.com>
2012-09-22 12:01:48 -0400

    tested all public method dummies for AQueue

--- src/AQueue/AQueue_test.cpp
+++ src/AQueue/AQueue_test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "AQueue.h"
 
 int main(){
   std::cout << "Hello World!!!!\n";
+  AQueue* q = new AQueue();
+  q->enqueue(1);
+  q->dequeue();
+  q->size();
+  q->isEmpty();
   return 0;
 }
